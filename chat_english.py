84b90ad2f1f21adfba68937d191791f8851859d1
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-06-28 13:16:10 +0200

    direct_llm_call

--- chat_english.py
+++ chat_english.py
@@ -9,4 +9,9 @@ dwani.api_base = os.getenv("DWANI_API_BASE_URL")
 response = dwani.Chat.direct(prompt="Hello!")
 print("Chat Response:  ",response)
 
+
+response = dwani.Vision.caption_direct(file_path="image.png",
+            query="Describe this image",model="gemma3"
+            )
+print("Vision Response:  ",response)
     
