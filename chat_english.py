dbedf80dd41324e97aff04a877e8ec549105ca33
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2025-06-28 07:36:21 +0200

    sachin harazrd (#2)
    * tst-user
    
    * init-hazrf
    
    * add-travcel
    
    * add-travel
    
    * add-api
    
    * base-c

---
+++ chat_english.py
@@ -0,0 +1,12 @@
+import dwani
+import os
+
+# Set API key and base URL
+dwani.api_key = os.getenv("DWANI_API_KEY")
+dwani.api_base = os.getenv("DWANI_API_BASE_URL")
+
+
+response = dwani.Chat.direct(prompt="Hello!")
+print("Chat Response:  ",response)
+
+    
